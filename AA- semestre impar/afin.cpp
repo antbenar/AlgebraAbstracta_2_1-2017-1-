4ea17ea3f4ec583bef02d5f7445dcd82cf50ff10
antbenar <antbenar1999@gmail.com>
2017-10-20 14:52:31 -0500

    Add files via upload

---
+++ AA- semestre impar/afin.cpp
@@ -0,0 +1,56 @@
+#include "afin.h"
+#include<string>
+#include "euclides.h"
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+afin::afin(){
+	alfabeto="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .,;";
+	generarClave();
+}
+
+afin::afin(int clave_a_, int clave_b_){
+	alfabeto="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .,;";
+	clave_a=clave_a_;
+	clave_b=clave_b_;
+}
+
+void afin::generarClave(){
+	srand (time(NULL));
+	lave_a=rand() % 100+1 ;//1-101
+	while(mcd(clave_a,alfabeto.size())!=1)
+		clave_a=rand() % 10000;
+	clave_b=rand() % 10000;
+	cout<<"a: "<<clave_a<<endl;
+	cout<<"b: "<<clave_b<<endl;
+}
+
+string afin::cifrar(string palabra){
+	int pos;
+	long long i;
+	for(i=0; i<palabra.size();i++){
+		pos=alfabeto.find(palabra[i])*clave_a;
+		pos+=clave_b;
+		
+		if(pos>=alfabeto.size()){
+			palabra[i]=alfabeto[MOD(pos,(int)alfabeto.size())];
+		}
+		palabra[i]=alfabeto[pos];
+	}
+	return palabra;
+}
+
+string afin::descifrar(string palabra){
+	vector<int>inversa = inversa(clave_a,alfabeto.size());
+	int pos;
+	for(int i=0; i<palabra.size();i++){
+		pos=(alfabeto.find(palabra[i])-clave_b);
+		if(pos<0)
+			MOD(pos,alfabeto.size());
+		pos=pos*inversa[1];
+		palabra[i]=alfabeto[MOD(pos,alfabeto.size())];
+	}
+	return palabra;
+}
---
+++ AA- semestre impar/cesar.cpp
@@ -0,0 +1,26 @@
+#include "Cesar.h"
+#include<string>
+#include "matematic.h"
+using namespace std;
+
+Cesar::Cesar(int clave_){
+	alfabeto="abcdefghijklmnopqrstuvwxyz ";
+	clave=clave_;
+}
+
+string Cesar::cifrar(string palabra){
+	int pos;
+	for(int i=0; i<palabra.size();i++){
+		pos=alfabeto.find(palabra[i])+clave;
+		palabra[i]=alfabeto[MOD(pos,alfabeto.size())];
+	}
+	return palabra;
+}
+string Cesar::descifrar(string palabra){
+	int pos;
+	for(int i=0; i<palabra.size();i++){
+		pos=alfabeto.find(palabra[i])-clave;
+		palabra[i]=alfabeto[MOD(pos,alfabeto.size())];
+	}
+	return palabra;
+}
