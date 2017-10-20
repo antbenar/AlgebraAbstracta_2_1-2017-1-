3d54455505d7e645eccbc8b521ef263ac7cc21bc
antbenar <antbenar1999@gmail.com>
2017-10-20 14:35:02 -0500

    Update Cesar.cpp

--- Cesar/src/Cesar.cpp
+++ Cesar/src/Cesar.cpp
@@ -4,29 +4,23 @@
 using namespace std;
 
 Cesar::Cesar(int clave_){
-    alfabeto="abcdefghijklmnopqrstuvwxyz ";
-    clave=clave_;
+	alfabeto="abcdefghijklmnopqrstuvwxyz ";
+	clave=clave_;
 }
 
 string Cesar::cifrar(string palabra){
-    int pos;
-    for(int i=0; i<palabra.size();i++){
-        pos=alfabeto.find(palabra[i])+clave;
-        if(pos>=alfabeto.size())
-            palabra[i]=alfabeto[MOD(pos,alfabeto.size())];
-        else
-            palabra[i]=alfabeto[pos];
-    }
-    return palabra;
+	int pos;
+	for(int i=0; i<palabra.size();i++){
+		pos=alfabeto.find(palabra[i])+clave;
+		palabra[i]=alfabeto[MOD(pos,alfabeto.size())];
+	}
+	return palabra;
 }
 string Cesar::descifrar(string palabra){
-        int pos;
-    for(int i=0; i<palabra.size();i++){
-        pos=alfabeto.find(palabra[i])-clave;
-        if(pos>=alfabeto.size())
-            palabra[i]=alfabeto[MOD(pos,alfabeto.size())];
-        else
-            palabra[i]=alfabeto[pos];
-    }
-    return palabra;
+	int pos;
+	for(int i=0; i<palabra.size();i++){
+		pos=alfabeto.find(palabra[i])-clave;
+		palabra[i]=alfabeto[MOD(pos,alfabeto.size())];
+	}
+	return palabra;
 }
